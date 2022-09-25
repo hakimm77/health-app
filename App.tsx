5bfad5874a2dc3857f9a25969359585f80f48a2b
hakimm77 <hakimboum406@gmail.com>
2022-09-25 23:40:59 +0100

    setup appollo and basic querries from server

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import UserPage from "./src/screen/UserPage";
 import { Provider as PaperProvider } from "react-native-paper";
 import { View } from "react-native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <PaperProvider>
+    <ApolloProvider client={client}>
       <UserPage />
-    </PaperProvider>
+    </ApolloProvider>
   );
 };
 
--- src/screen/UserPage.tsx
+++ src/screen/UserPage.tsx
@@ -1,9 +1,28 @@
 import { View, StyleSheet, Text } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
+
+const GET_WEIGHTS = gql`
+  query GET_USER_WEIGHTS {
+    userWeights {
+      value
+    }
+  }
+`;
 
 const UserPage = () => {
+  const { loading, error, data } = useQuery(GET_WEIGHTS);
+
+  useEffect(() => {
+    console.log(data);
+  }, [loading, error, data]);
+
   return (
     <View style={styles.container}>
-      <Text>heybye</Text>
+      {data &&
+        data.userWeights.map((item: any, idx: any) => (
+          <Text key={idx}>{item.value}</Text>
+        ))}
     </View>
   );
 };
@@ -14,6 +33,7 @@ const styles = StyleSheet.create({
     height: "100%",
     backgroundColor: "#ffe7e8",
     alignItems: "center",
+    padding: 20,
   },
 });
 
